e743d91caf413cb0cd711edbe9de7eaa52279477
doanhdai <doanhdaigr5.2004@gmail.com>
2025-02-27 10:23:03 +0700

    buid project

---
+++ src/main/java/com/example/backend_instagram/BackendInstagramApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_instagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendInstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendInstagramApplication.class, args);
+	}
+
+}
